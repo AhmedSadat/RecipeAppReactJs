5ccb9ae81151d78c27c0112393150bcdc1d8dc29
Ahmed Sadat <ahmedsadat1995@gmail.com>
2019-12-30 20:45:53 +0200

    intialize our app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+               <div>hi from food2fork<i class="fa fa-book" aria-hidden="true"></i></div>
+    );
+  }
+}
+
+export default App;
