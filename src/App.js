b75c880f4181454c0c9eaa14d1af5576aa46ba47
Ahmed Sadat <ahmedsadat1995@gmail.com>
2020-01-04 03:54:20 +0200

    component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+import SingleRecipe from './components/Recipes/SingleRecipe/SingleRecipe' ;
+import Recipes from './components/Recipes/Recipes' ;
+import DefaultPage from './components/DefaultPage/DefaultPage' ;
+import { BrowserRouter , Route , Switch } from 'react-router-dom';
+import Home from './components/Home/Home' ;
+import Navbar from './components/navbar/navbar';
 
 class App extends Component {
   render() {
-    return (
-               <div>hi from food2fork<i class="fa fa-book" aria-hidden="true"></i></div>
+    return (   
+        <BrowserRouter>
+        <Navbar />
+               <main>
+                 <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/home" component={Home} />
+                    <Route path="/recipes" component={Recipes} />
+                    <Route path="/recipes/:id" component={SingleRecipe} />
+                    <Route component={DefaultPage} />
+                  </Switch>
+               </main>
+       </BrowserRouter>       
     );
   }
 }
